e0d23eacdc743930e15c0f3e1835a1e3f9b7b08e
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-07-01 19:26:32 +0300

    E-commerce App | Form component implemented for ordering in Cart Page.

--- client/src/pages/Cart/index.jsx
+++ client/src/pages/Cart/index.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import { Modal } from 'antd';
+import { Form, Input, Table, Button } from 'antd';
+
+import { useState } from 'react';
 import Navbar from '../../components/Navbar/index.jsx';
 
-import { Space, Table, Tag } from 'antd';
 
 const Cart = () => {
+    const dataSource = [
+        {
+            key: '1',
+            name: 'Mike',
+            age: 32,
+            address: '10 Downing Street',
+        },
+        {
+            key: '2',
+            name: 'John',
+            age: 42,
+            address: '10 Downing Street',
+        },
+        {
+            key: '3',
+            name: 'Mary',
+            age: 27,
+            address: '10 Cycle Street',
+        },
+    ];
+
     const columns = [
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
-            render: (text) => <a>{text}</a>,
         },
         {
             title: 'Age',
@@ -21,80 +44,23 @@ const Cart = () => {
             dataIndex: 'address',
             key: 'address',
         },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map((tag) => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <a>Invite {record.name}</a>
-                    <a>Delete</a>
-                </Space>
-            ),
-        },
     ];
-    const data = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    ];
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+    // const onFinishFailed = (errorInfo) => {
+    //     console.log('Failed:', errorInfo);
+    // };
+
     return (
         <div>
             <Navbar />
             <div className='cartpage'>
-                <Table className="cart_table" columns={columns} dataSource={data} pagination={false} />
+                {/* <Table  columns={columns} dataSource={data} pagination={false} /> */}
+                <Table className="cart_table" dataSource={dataSource} columns={columns} pagination={false} />
                 <div className='bill'>
                     <h2>Order</h2>
                     <div className='bill_info'>
@@ -104,8 +70,78 @@ const Cart = () => {
                     <div className='bill_total'>
                         3.624 TL
                     </div>
-                    <button className='confirm_btn'>Confirm the Order</button>
+                    <button className='confirm_btn' onClick={() => setIsModalOpen(true)}>Confirm the Order</button>
                 </div>
+                <Modal title="Order" footer={false} open={isModalOpen} onOk={() => setIsModalOpen(false)} onCancel={() => setIsModalOpen(false)}>
+                    <Form
+                        layout='vertical'
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
+                        // onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            label="Name"
+                            name="name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your name!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Surname"
+                            name="surname"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your surname!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name={['user', 'email']}
+                            label="Email"
+                            rules={[
+                                {
+                                    type: 'email',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Address"
+                            name="address"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your address!',
+                                },
+                            ]}
+                        >
+                            <Input.TextArea />
+                        </Form.Item>
+                        <div className='bill'>
+                            <div className='bill_info'>
+                                <div>Subtotal<span>3.421 TL</span></div>
+                                <div>Cargo<span>203 TL</span></div>
+                            </div>
+                            <div className='bill_total'>
+                                3.624 TL
+                            </div>
+                            <Button className='confirm_btn' htmlType='submit' onClick={() => setIsModalOpen(false)} >Confirm the Order</Button>
+                        </div>
+                    </Form>
+                </Modal>
             </div>
         </div>
     )
