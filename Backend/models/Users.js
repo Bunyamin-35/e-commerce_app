09de8a0e30ed80e57fb0643ba2c3570585b92569
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-07-07 16:16:10 +0300

    E-commerce App | bcrypt used for hashing the users' passwords.

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
 
 const { Schema } = mongoose;
 
@@ -24,6 +25,12 @@ const userSchema = new Schema(
         timestamps: true
     }
 );
-
+userSchema.pre('save',function(next){
+    const user = this;
+    bcrypt.hash(user.password,10,(error,hash)=>{
+        user.password = hash;
+        next();
+    });
+});
 const User = mongoose.model("User", userSchema);
 export default User;
